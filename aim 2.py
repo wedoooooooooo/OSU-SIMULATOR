5f26ada51fda09d34a664346995508469502f9c6
wedoooooooooo <ductuti2216@gmail.com>
2026-06-30 00:33:37 +0700

    feat!: added a new button on the top right of the screen that alternates between different sprites and window size when the player left clicks.
    feat: linked the button states (pressed/unpressed) to a bool value for clearer management of the button's states and individual functionalities of each state.

--- aim 2.py
+++ aim 2.py
@@ -41,6 +41,17 @@ class Ball(pygame.sprite.Sprite):
 sprite_ball = pygame.sprite.GroupSingle()
 ball_real = Ball(sprite_ball, pos_x=100, pos_y=100)
 
+class Button(pygame.sprite.Sprite):
+    def __init__(self, *groups, pos_x, pos_y, pressed_state):
+        super().__init__(*groups)
+        self.image = pygame.image.load(pressed_state).convert_alpha()
+        self.rect = self.image.get_rect(center = (pos_x,pos_y))
+
+sprite_button = pygame.sprite.Group()
+button = Button(sprite_button, pos_x=500, pos_y=70, pressed_state='graphics1/buttonunpressed.png')
+# sets the default state of the button to a bool value (False) of a variable (toggle), in which the variable will later be toggled to activate certain behaviours
+toggle = False 
+
 ## the count variable here is actually really important because it will be the integer display of the scoring system
 count = 0
 
@@ -51,6 +62,8 @@ while True:
 
     # turns the background white
     screen.fill('White')
+
+    sprite_button.draw(screen)
 
     # you cant blit a font render outside of 'while True', sad
     score = test_font.render('Score: ' + str(count), True, 'black')
@@ -79,10 +92,27 @@ while True:
         if event.type == pygame.MOUSEMOTION:
             player_real = Player(sprite_player, pos_x=(pygame.mouse.get_pos()[0]), pos_y=(pygame.mouse.get_pos()[1]))
 
+        # if the player cursor collides with the button:
+        if pygame.sprite.collide_rect(player_real, button):
+            # if the player collides with the button and left click:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                ## there are two states of the button which are determined by an alternating bool value (dependent on left clicks) 
+                # this is for the default state of the button (FALSE)
+                if toggle:
+                    toggle = False
+                    button = Button(sprite_button, pos_x=500, pos_y=70, pressed_state='graphics1/buttonunpressed.png')
+                    screen = pygame.display.set_mode((600,600))
+                # this is for the changed state of the button (TRUE)
+                else:
+                    toggle = True
+                    button = Button(sprite_button, pos_x=500, pos_y=70, pressed_state='graphics1/buttonpressed.png')
+                    # changes window size
+                    screen = pygame.display.set_mode((750,600))
+
         ## checks when the player's mouse pos (or the player sprite that follows the pos) collides with the ball/circle
         if pygame.sprite.collide_rect(player_real, ball_real):
 
-            ## (line 84, 85) if the player's mouse pos is colliding with the ball and the game receives an X or Z input, then the following happens:
+            ## if the player's mouse pos is colliding with the ball and the game receives an X or Z input, then the following happens:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_x or event.key == pygame.K_z:
 
@@ -92,7 +122,7 @@ while True:
                     x = float(random.randint(7,55) * random.randint(8,10))
                     y = float(random.randint(7,55) * random.randint(8,10))
 
-                    # line 98 prints the new location of the ball, just to ensure that the janky code works xd (just uncomment to use)
+                    # the line below prints the new location of the ball, just to ensure that the janky code works xd (just uncomment to use)
                     # print("(" + str(x) + ", " + str(y) + ")")
 
                     # fetches the ball sprite and puts it in the new random position                    
